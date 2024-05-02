5e551788cad2074355622512fb1d2fd3ab4b9625
paangulo6 <paulalardero581@gmail.com>
2024-05-02 13:07:42 +0200

    Chat

--- app/src/main/java/com/example/padelhub/ui/screens/PadelScreens_Chat.kt
+++ app/src/main/java/com/example/padelhub/ui/screens/PadelScreens_Chat.kt
@@ -8,35 +8,41 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-
-/**
- * Composable principal que sirve como contenedor
- * que despliega el contenido dependiendo del estado de la interfaz
- * y el tamaño de la ventana
- */
-
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.paint
@@ -47,10 +53,15 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import com.example.padelhub.R
-import com.example.padelhub.ui.navigation.AppScreens
-import com.example.padelhub.ui.theme.verdePadel
+
+import com.example.padelhub.ui.screens.BottomNavigation
+import com.example.padelhub.ui.theme.PadelHubTheme
+
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
@@ -62,15 +73,52 @@ fun HomeScreenChat(navController: NavController) {
     }
 }
 
+data class Message(val type: MessageType, val content: Any) {
+    enum class MessageType {
+        TEXT
+    }
+}
+@Composable
+fun MessageItem(message: Message) {
+    when (message.type) {
+        Message.MessageType.TEXT -> {
+            TextMessage(message.content as String)
+        }
+    }
+}
+
+@Composable
+fun TextMessage(text: String, color: Color = Color.White) {
+    Row(
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.person_24px),
+            contentDescription = null,
+            modifier = Modifier.size(40.dp)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(
+            text = text,
+            color = color,
+            modifier = Modifier.align(Alignment.CenterVertically)
+        )
+    }
+}
+
 @Composable
 fun ContenidoAppChat() {
+    var messageText by remember { mutableStateOf(TextFieldValue()) }
+    var messages by remember { mutableStateOf(listOf<Message>()) }
+
     val backgroundImage: Painter = painterResource(id = R.drawable.fondo)
     Box(modifier = Modifier
         .fillMaxSize()
         .paint(
             // Replace with your image id
             painterResource(id = R.drawable.fondo),
-            contentScale = ContentScale.FillBounds)
+            contentScale = ContentScale.FillBounds
+        )
     ) {
         Image(
             painter = backgroundImage,
@@ -103,8 +151,58 @@ fun ContenidoAppChat() {
                     textAlign = TextAlign.Start
                 )
             }
+
+            Scaffold(
+                containerColor = Color.Transparent,
+                floatingActionButton = {
+                    FloatingActionButton(
+                        onClick = {
+                            // Acción para enviar mensaje
+                            if (messageText.text.isNotEmpty()) {
+                                messages = messages + Message(Message.MessageType.TEXT, messageText.text)
+                                messageText = TextFieldValue()
+                            }
+                        }
+                    ) {
+                        Icon(Icons.Filled.Send, contentDescription = "Enviar")
+                    }
+                },
+                floatingActionButtonPosition = FabPosition.End
+            ) { innerPadding ->
+                Column(
+                    modifier = Modifier.padding(innerPadding)
+                ) {
+                    LazyColumn(
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        items(messages) { message ->
+                            MessageItem(message = message)
+                        }
+                    }
+                    Row(
+                        modifier = Modifier.padding(50.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Spacer(modifier = Modifier.width(8.dp))
+                        TextField(
+                            value = messageText,
+                            onValueChange = {
+                                messageText = it
+                            },
+                            modifier = Modifier.weight(1f)
+                        )
+                    }
+                }
+            }
+
         }
     }
 }
 
-
+@Preview
+@Composable
+fun PadelHubPreview(){
+    PadelHubTheme {
+        HomeScreenChat(navController = rememberNavController())
+    }
+}
